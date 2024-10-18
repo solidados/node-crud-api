152f77c8d2a8b469124b1319a04d6bb1fd410162
solidados <geekbrains.pk@internet.ru>
2024-10-19 00:35:49 +0400

    chore: update userModel by moving IRequestBody from userController

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,3 +4,9 @@ export interface IUser {
   age: number;
   hobbies: string[];
 }
+
+export interface IRequestBody extends Omit<IUser, 'id'> {
+  username: string;
+  age: number;
+  hobbies: string[];
+}
