db20285859b1c55ea74fd1bfbbefb3e88c4e8626
solidados <geekbrains.pk@internet.ru>
2024-10-19 19:10:10 +0400

    feat: add CORS-headers for any possible future use in browser applications

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,26 @@ const requestHandler = async (
   res: ServerResponse,
 ): Promise<void> => {
   try {
+    // Here I set CORS-Headers only as a bonus in case this API might be used in browser applications
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
     const protocol: string | string[] =
       req.headers['x-forwarded-proto'] || 'http';
     const url = new URL(req.url || '', `${protocol}://${req.headers.host}`);
     const pathname: string = url.pathname.replace(/\/$/, '');
 
     const isHandled: boolean = await handleUserRoutes(req, res, pathname);
-    if (!isHandled) notFoundHandler(res);
+    if (!isHandled) {
+      notFoundHandler(res);
+      return;
+    }
   } catch {
     serverErrorHandler(res);
   }
