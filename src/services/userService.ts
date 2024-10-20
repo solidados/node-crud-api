ec231cb19bad5fd49e72aa3a653e07ae910ddca6
solidados <geekbrains.pk@internet.ru>
2024-10-21 01:27:36 +0400

    chore: update userService.ts to work with sharedMemory.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,16 +1,27 @@
 import { IUser } from '../models/userModel';
 import { v4 } from 'uuid';
+import { sharedMemory } from '../sharedMemory';
 
-const users: IUser[] = [];
+const USERS_KEY: string = 'users';
 
-export const getAllUsers = async (): Promise<IUser[]> => users;
+const getUsers = (): IUser[] => {
+  const data = sharedMemory.get(USERS_KEY);
+  return Array.isArray(data) ? (data as IUser[]) : [];
+};
 
-export const getUserById = async (id: string): Promise<IUser | undefined> =>
-  users.find((user: IUser): boolean => user.id === id);
+const setUsers = (users: IUser[]) => sharedMemory.set(USERS_KEY, users);
+
+export const getAllUsers = async (): Promise<IUser[]> => getUsers();
+
+export const getUserById = async (id: string): Promise<IUser | undefined> => {
+  return getUsers().find((user: IUser): boolean => user.id === id);
+};
 
 export const createUser = async (data: Omit<IUser, 'id'>): Promise<IUser> => {
   const newUser: IUser = { id: v4(), ...data };
+  const users = getUsers();
   users.push(newUser);
+  setUsers(users);
   return newUser;
 };
 
@@ -18,6 +29,7 @@ export const updateUser = async (
   id: string,
   data: Partial<Omit<IUser, 'id'>>,
 ): Promise<IUser | null> => {
+  const users = getUsers();
   const userIndex: number = users.findIndex(
     (user: IUser): boolean => user.id === id,
   );
@@ -26,11 +38,13 @@ export const updateUser = async (
   }
 
   users[userIndex] = { ...users[userIndex], ...data };
+  setUsers(users);
 
   return users[userIndex];
 };
 
 export const deleteUser = async (id: string): Promise<boolean> => {
+  const users = getUsers();
   const index: number = users.findIndex(
     (user: IUser): boolean => user.id === id,
   );
@@ -39,6 +53,7 @@ export const deleteUser = async (id: string): Promise<boolean> => {
   }
 
   users.splice(index, 1);
+  setUsers(users);
 
   return true;
 };
