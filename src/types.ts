92347eccc1a1d0a9ab2b288472337c681daf8610
solidados <geekbrains.pk@internet.ru>
2024-10-21 00:55:23 +0400

    chore: update-2 types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,9 @@
-/* eslint-disable no-unused-vars */
 export enum HttpMethod {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
   DELETE = 'DELETE',
 }
-/* eslint-enable no-unused-vars */
 
 export interface RequestMessage {
   method: string | undefined;
