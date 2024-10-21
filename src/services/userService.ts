6c8847008bb803e299efec7ba4cf0c36062ba75f
solidados <geekbrains.pk@internet.ru>
2024-10-21 21:20:49 +0400

    refactor: update userService.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,38 +1,8 @@
-// import cluster from 'node:cluster';
 import { v4 } from 'uuid';
 import { IUser } from '../models/userModel';
 import { sharedMemory } from '../sharedMemory';
-// import { Message } from '../types';
-// import { sharedMemory } from '../sharedMemory';
 
 const USERS_KEY: string = 'users';
-
-/*const sendMessageToPrimary = <T>(
-  action: string,
-  key: string,
-  value?: T,
-): Promise<T | undefined> => {
-  return new Promise((resolve) => {
-    if (cluster.isWorker && process.send) {
-      const message: Message<T> = { action, key, value };
-      process.send(message);
-
-      process.once('message', (response: Message<T> | unknown) => {
-        if (
-          typeof response === 'object' &&
-          response !== null &&
-          'value' in response
-        ) {
-          resolve((response as Message<T>).value);
-        } else {
-          resolve(undefined);
-        }
-      });
-    } else {
-      resolve(undefined);
-    }
-  });
-};*/
 
 const getUsers = async (): Promise<IUser[]> => {
   const data = (await sharedMemory.get(USERS_KEY)) as IUser[] | undefined;
